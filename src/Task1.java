8630c84b3b9fa54e71d1d1bf05c5d10eb99c3590
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-06-09 08:12:36 +0200

    create correct working with normal element set

--- src/Task1.java
+++ src/Task1.java
@@ -7,6 +7,34 @@ public class Task1 {
   //Increment the large integer by one and return the resulting array of digits.
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int[] example = {1, 2, 3, 9};
+    int[] check = plusOne(example);
+  }
+
+  public static int[] plusOne(int[] digits) {
+    //к нам заходит массив digits.
+    // Значит мне нужно:
+    // 1. + определить длинну массива.
+    // 2. + найти последний элемент.
+    // 3. проверить, являеться ли этот элемент числом 9
+    // 4. прибавить к этому элементу единицу. учесть граничный случай с числом 9
+    // 4. Вернуть элемент в конец массива вместь имеющегося
+
+    int numbersOfElement = digits.length;
+    int lastElement = digits[(numbersOfElement - 1)];
+    for (int i = numbersOfElement - 1; i >= 0; i--) {
+      if (digits[i] == 9) {
+        digits[i] = 0;
+        digits[i - 1] = digits[i - 1] + 1;
+      } else {
+        digits[i] += 1;
+        break;
+      }
+    }
+    for (int elements : digits) {
+      System.out.println(elements);
+    }
+
+    return digits;
   }
 }
